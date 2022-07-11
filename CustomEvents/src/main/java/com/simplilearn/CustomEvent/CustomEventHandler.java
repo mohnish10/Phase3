be5482989d3ce75ff8854955057d04d9e248e387
Mohnish <mohnishpwr10@gmail.com>
2022-07-11 01:29:08 -0400

    All messages have been committed

---
+++ CustomEvents/src/main/java/com/simplilearn/CustomEvent/CustomEventHandler.java
@@ -0,0 +1,15 @@
+package com.simplilearn.CustomEvent;
+
+import org.springframework.context.ApplicationListener;
+
+public class CustomEventHandler implements ApplicationListener<CustomEvent>
+{
+
+	public void onApplicationEvent(CustomEvent event) {
+System.out.println(event.toString());
+		
+	}
+
+
+
+}
---
+++ EventHandlingWeb/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="default">Click here</a>
+<br><br>
+<a href="custom">Click here Custom</a>
+</body>
+</html>
